7398ab5475faa24d0f614885d2f5b446821f172b
StevenVallarsa <orionsjv@gmail.com>
2022-01-20 21:41:17 -0500

    Completed exercise

--- src/main/java/com/sg/jdbctemplateexample/App.java
+++ src/main/java/com/sg/jdbctemplateexample/App.java
@@ -1,15 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.sg.jdbctemplateexample;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 /**
  *
@@ -19,6 +20,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App implements CommandLineRunner {
 
     private static Scanner sc;
+    
+    // Creates the JDBC Template Bean for us behind the scenes
+    @Autowired
+    private JdbcTemplate jdbc;
 
     public static void main(String args[]) {
         SpringApplication.run(App.class, args);
@@ -58,6 +63,7 @@ public class App implements CommandLineRunner {
                     default:
                         System.out.println("I don't understand");
                 }
+                System.out.println("");
             } catch (Exception ex) {
                 System.out.println("Error communicating with database");
                 System.out.println(ex.getMessage());
@@ -68,19 +74,92 @@ public class App implements CommandLineRunner {
     }
 
     private void displayList() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<ToDo> todos = jdbc.query("SELECT * FROM todo", new ToDoMapper());
+        for (ToDo td : todos) {
+            System.out.printf("%s: %s -- %s -- %s%n", 
+            td.getId(),
+            td.getTodo(),
+            td.getNote(),
+            td.isFinished());
+        }
+        System.out.println("");
     }
 
     private void addItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Add Item");
+        System.out.println("What is the task?");
+        String task = sc.nextLine();
+        System.out.println("Any additional notes?");
+        String note = sc.nextLine();
+        
+        jdbc.update("INSERT INTO todo(todo, note) VALUES(?,?)", task, note);
+        System.out.println("Add Complete");
     }
 
     private void updateItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Update Item");
+        System.out.println("Which item do you want to update");
+        String itemId = sc.nextLine();
+        ToDo item = jdbc.queryForObject("SELECT * FROM todo WHERE id = ?", new ToDoMapper(), itemId);
+        
+        System.out.println("1. ToDo - " + item.getTodo());
+        System.out.println("2. Note - " + item.getNote());
+        System.out.println("3. Finished - " + item.isFinished());
+        
+        System.out.println("\nWhat would you like to change?");
+        String choice = sc.nextLine();
+        
+        switch(choice) {
+            case "1":
+                System.out.println("Enter new ToDo:");
+                String todo = sc.nextLine();
+                item.setTodo(todo);
+                break;
+            case "2":
+                System.out.println("Enter new Note:");
+                String note = sc.nextLine();
+                item.setNote(note);
+                break;
+            case "3":
+                System.out.println("Toggling Finished to " + !item.isFinished());
+                item.setFinished(!item.isFinished());
+                break;
+            default:
+                System.out.println("No changes made!");
+//                return;
+        }
+        System.out.println("");
+        
+        jdbc.update("UPDATE todo SET todo = ?, note = ?, finished = ? WHERE id = ?",
+                item.getTodo(),
+                item.getNote(),
+                item.isFinished(),
+                item.getId());
+        System.out.println("Update Complete");
+        
     }
 
     private void removeItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Remove Item");
+        System.out.println("Which item would you like to remove?");
+        String itemId = sc.nextLine();
+        jdbc.update("DELETE FROM todo WHERE id = ?", itemId);
+        System.out.println("Remove Complete\n");
+        
+    }
+    
+    private static final class ToDoMapper implements RowMapper<ToDo> {
+
+        @Override
+        public ToDo mapRow(ResultSet rs, int i) throws SQLException {
+            ToDo todo = new ToDo();
+            todo.setId(rs.getInt("id"));
+            todo.setTodo(rs.getString("todo"));
+            todo.setNote(rs.getString("note"));
+            todo.setFinished(rs.getBoolean("finished"));
+            return todo;
+        }
+        
     }
 
 }
